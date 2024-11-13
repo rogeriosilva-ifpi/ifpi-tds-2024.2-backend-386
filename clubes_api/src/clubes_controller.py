7ccf1424ce785dee832c7ead742fca8cf409a59c
Rogério Silva <rogerio410@gmail.com>
2024-11-13 20:13:39 -0300

    SQLModel CRUD

--- clubes_api/src/clubes_controller.py
+++ clubes_api/src/clubes_controller.py
@@ -6,14 +6,6 @@ from .database import get_engine
 
 router = APIRouter()
 
-# Instancia(objeto) de Clube
-clubes = [
-    Clube(id=1, nome='Santos', uf='SP', serie='B', ano=1900),
-    Clube(id=2, nome='Fortaleza', uf='CE', serie='A', ano=1970),
-    Clube(id=3, nome='Ceará', uf='CE', serie='B', ano=1958),
-    Clube(id=4, nome='Palmeiras', uf='SP', serie='A', ano=1909)
-  ]
-
 
 @router.get('', status_code=status.HTTP_200_OK)
 def clubes_list(uf: str | None = None, serie: str | None = None):
@@ -30,21 +22,20 @@ def clubes_list(uf: str | None = None, serie: str | None = None):
   clubes = session.exec(statement).all()
   return clubes
 
- 
 
 @router.get('/{id}')
 def clube_detail(id: int):
-  for clube in clubes:
-    if clube.id == id:
-      return clube
+  with Session(get_engine()) as session:
+    try:
+      return get_clube_by_id(id, session)
+    except:
+      raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail='Clube não localizado!'
+        )
 
-  raise HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND,
-    detail='Clube não localizado!'
-    )
 
-
-@router.post('/', status_code=status.HTTP_201_CREATED)
+@router.post('', status_code=status.HTTP_201_CREATED)
 def clubes_create(novo_clube: Clube):
   session = Session(get_engine())
   session.add(novo_clube)
@@ -55,35 +46,41 @@ def clubes_create(novo_clube: Clube):
 
 @router.put('/{id}')
 def clube_edit(id: int, clube_editado: Clube):
-  index = obter_index_clube_por_id(id)
+    with Session(get_engine()) as session:
+      try:
+        clube = get_clube_by_id(id, session)
+        clube.nome = clube_editado.nome
+        clube.serie = clube_editado.serie
+        clube.ano = clube_editado.ano
+        clube.uf = clube_editado.uf
 
-  if index != None:
-    clubes[index] = clube_editado
-    return
-  else:
-    raise HTTPException(
-      status_code=status.HTTP_404_NOT_FOUND,
-      detail='Clube não localizado!'
-    )
+        session.add(clube)
+        session.commit()
+        session.refresh(clube)
+        return clube
+      except:
+        raise HTTPException(
+          status_code=status.HTTP_404_NOT_FOUND,
+          detail='Clube não localizado!'
+        )
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def clube_remove(id: int):
-  index = obter_index_clube_por_id(id)
+  with Session(get_engine()) as session:
+    try:
+      clube = get_clube_by_id(id, session)
+      session.delete(clube)
+      session.commit()
+      return
+    except:
+      raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail='Clube não localizado!'
+      )
 
-  if index != None:
-    clubes.pop(index)
-    return
-  else:
-    raise HTTPException(
-      status_code=status.HTTP_404_NOT_FOUND,
-      detail='Clube não localizado!'
-    )
 
-# Utils
-def obter_index_clube_por_id(id: int):
-  for i in range(len(clubes)):
-    if clubes[i].id == id:
-      return i
-    
-  return None
+def get_clube_by_id(id: int, session: Session):
+  statement = select(Clube).where(Clube.id == id)
+  clube = session.exec(statement).one()
+  return clube
--- clubes_api/src/database.py
+++ clubes_api/src/database.py
@@ -1,10 +1,13 @@
 from sqlmodel import SQLModel, create_engine
 
 def get_engine():
-  engine = create_engine('sqlite:///base_clubes.db')
+  # engine = create_engine('sqlite:///base_clubes.db')
+
+  engine = create_engine('postgresql://postgres:postgres@localhost:5432/clubes_386')
 
   return engine
 
 
 def init_db():
+  # Cria as tabelas em BD para cada Classe SQLModel 
   SQLModel.metadata.create_all(get_engine())

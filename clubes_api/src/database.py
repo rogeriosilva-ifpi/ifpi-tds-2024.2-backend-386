f7d83243da3aea2907530772409a04dcc9d10bfe
Rogério Silva <rogerio410@gmail.com>
2024-12-04 19:24:10 -0300

    Ambiente

--- clubes_api/src/database.py
+++ clubes_api/src/database.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel, create_engine
 
 def get_engine():
   # engine = create_engine('sqlite:///base_clubes.db')
-
+                        # postgresql://usuario:senha@endereco_host:port/nome_banco
   engine = create_engine('postgresql://postgres:postgres@localhost:5432/clubes_386')
 
   return engine
